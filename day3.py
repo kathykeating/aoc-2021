02cfb57f2f801e02863bdb92e431bb56a79384d5
Kathy Keating <kathkeating+github@gmail.com>
2021-12-03 15:05:09 -0700

    calculate the Life Support Rating

--- day3.py
+++ day3.py
@@ -7,15 +7,21 @@ def main_day3():
     sub = submarine.Submarine()
 
     # DAY 3 ----------------------------------------------------------
-    print('\n\n***** DAY 2 *****')
-    # process location after movement
+    print('\n\n***** DAY 3 *****')
+    # process determine power consumption
     diagnostics = utils.read_file("./data/diagnostics.csv", "field", ' ')
-    # print(diagnostics)
-    diagnostic = sub.determine_power(diagnostics)
+    diagnostic = sub.Diagnostic(diagnostics)
+    sub.determine_power(diagnostic)
 
     print('\nPower Consumption:\n  Gamma = {0}\n  Epsilon = {1}\n  Power = {2}'
-          .format(diagnostic.gamma, diagnostic.epsilon, diagnostic.power_consumption))
+          .format(diagnostic.gamma, diagnostic.epsilon, sub.power_consumption))
 
+    # verify the life support rating determined by the oxygen generator rating
+    sub.oxygen_generator_rating = diagnostic.determine_oxygen_generator_rating()
+    sub.co2_scrubber_rating = diagnostic.determine_co2_scrubber_rating()
+    sub.life_support_rating = sub.oxygen_generator_rating * sub.co2_scrubber_rating
 
-
-
+    print('\nLife Support Rating:\n  Oxygen Generator = {0}\n  CO2 Scrubber = {1}\n  Rating = {2}'
+          .format(sub.oxygen_generator_rating,
+                  sub.co2_scrubber_rating,
+                  sub.life_support_rating))
--- submarine.py
+++ submarine.py
@@ -5,7 +5,10 @@ import utils
 
 class Submarine:
     def __init__(self):
-        foobar = 1
+        self.power_consumption = 0
+        self.oxygen_generator_rating = 0
+        self.co2_scrubber_rating = 0
+        self.life_support_rating = 0
 
     class Location:
       def __init__(self, forward, depth):
@@ -19,10 +22,71 @@ class Submarine:
         self.aim = aim
 
     class Diagnostic:
-        def __init__(self):
+        def __init__(self, diagnostics):
             self.gamma = ""
             self.epsilon = ""
-            self.power_consumption = 0
+            self.diagnostics = diagnostics
+            self.max = self.determine_max(diagnostics)
+
+        # calculate the most common digit in each column of the set
+        def determine_max(self, list):
+            max = [0] * len(list[0])
+            ones = [0] * len(list[0])
+            zeros = [0] * len(list[0])
+            for idx, reading in enumerate(list):
+                # print(reading, len(reading))
+                for l, bit in enumerate(reading):
+                    if int(reading[l]) == 1:
+                        max[l] += 1
+                        ones[l] += 1
+                    else:
+                        max[l] -= 1
+                        zeros[l] += 1
+
+            # print("ones=", ones)
+            # print("zeros=", zeros)
+            return max
+
+
+        def match_method(self, method, filter):
+            # set the match method
+            if method == "max":
+                match = 0
+                if filter >= 0:  # if equal, use one in "max" algorithm
+                    match = 1
+            else:
+                match = 1
+                if filter >= 0:  # if equal, use zero in "min" algorithm
+                    match = 0
+
+            return match
+
+        def filter_list(self, list, method, column):
+            newlist = []
+            filter = self.determine_max(list)
+            match = self.match_method(method, filter[column])
+
+            for item in list:
+                if int(item[column]) == match:
+                    newlist.append(item)
+
+            if len(newlist) == 1:
+                return newlist
+            else:
+                return self.filter_list(newlist,  method, column + 1)
+
+        # determine the oxygen generator rating from the diagnostics
+        def determine_oxygen_generator_rating(self):
+
+            list = self.filter_list(self.diagnostics, "max", 0)
+            return int(list[0], 2)
+
+        # determine the CO2 scrubber rating from the diagnostics
+        def determine_co2_scrubber_rating(self):
+
+            list = self.filter_list(self.diagnostics, "min", 0)
+            return int(list[0], 2)
+
 
     # Determine the Location
     def determine_location(self, movement):
@@ -53,20 +117,9 @@ class Submarine:
 
 
     # Determine the gamma within the diagnostics
-    def determine_power(self, diagnostics):
-        diagnostic = self.Diagnostic()
-
-        max = [0] * len(diagnostics[0])
-        for idx, reading in enumerate(diagnostics):
-            # print(reading, len(reading))
-            for l, bit in enumerate(reading):
-                if int(reading[l]) == 1:
-                    max[l] += 1
-                else:
-                    max[l] -= 1
-
+    def determine_power(self, diagnostic):
         # set the bit
-        for idx, bit in enumerate(max):
+        for idx, bit in enumerate(diagnostic.max):
             if int(bit) >= 0:
                 diagnostic.gamma += str(1)
                 diagnostic.epsilon += str(0)
@@ -74,6 +127,6 @@ class Submarine:
                 diagnostic.gamma += str(0)
                 diagnostic.epsilon += str(1)
 
-        diagnostic.power_consumption = int(diagnostic.gamma,2) * int(diagnostic.epsilon,2)
+        self.power_consumption = int(diagnostic.gamma,2) * int(diagnostic.epsilon,2)
 
-        return diagnostic
+
